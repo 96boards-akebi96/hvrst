a41307108d29e173f5cdca8a00aea6af3defb72c
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2019-06-11 13:39:43 +0900

    Add License and Copyright notices
    - Add SPDX-License-Identifier
    - Add Copyright notice
    
    Change-Id: I3257abfa6b31c503e0a715d1eb20f389653d7e15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2018 Socionext Inc.
 
 TARGET_RECOVERY_ADD_SYSTEM_BIN += \
 	    $(TARGET_COPY_OUT_VENDOR)/bin/hvrst
--- hvrst.cpp
+++ hvrst.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2018 Socionext Inc.
+
 #include <unistd.h>
 
 #define LOG_TAG "hvrst"
